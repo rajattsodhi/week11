53d0d82d66e892e1658a789a1190973bf2c8c684
Rajatt Sodhi <P14166609@LEC-ADMIN.dmu.ac.uk>
2016-12-13 09:44:56 +0000

    added clear storage and done the optional task

--- js/localstore.js
+++ js/localstore.js
@@ -4,12 +4,20 @@
   let getName = document.getElementById("get-name");
   let userName = document.getElementById("user-name"); 
   let nameStored = localStorage.name;
+  let btn = document.getElementById("clear-storage");
+  
+  let mySurname = document.getElementById("my-surname");
+  let surname = document.getElementById("surname"); 
+  let surnameStored = localStorage.surname;
   console.log(`Name on page load: ${nameStored}`);
+  console.log(`Surname on page load: ${surnameStored}`);
   
   if(nameStored) {
     // If there's a name in localStorage, use it:
     myName.innerHTML = `again ${nameStored}`;
+    mySurname.innerHTML = ` ${surnameStored}`;
     console.log(`Name stored is: ${nameStored}`);
+    console.log(`Surname stored is: ${surnameStored}`);
   }
   else {
     // There's no name in localStorage:
@@ -22,13 +30,21 @@
       alert("Please enter a name");
       userName.focus();
     }
+    if(surname.value === "") {
+      alert("Please enter a surname");
+      surname.focus();
+    }
     // Get the name the user entered:
     nameStored = userName.value;
+    surnameStored = surname.value;
     // Show the name in "my-name":
     myName.innerHTML = nameStored;
+    mySurname.innerHTML = surnameStored;
     // Put the name into localStorage:
     localStorage.name = nameStored;
+    localStorage.surname = surnameStored;
     console.log(`New name stored: ${nameStored}`);
+    console.log(`New surname stored: ${surnameStored}`);
     return false;
   }
 
@@ -40,5 +56,11 @@
     getName.addEventListener("submit", PerformGreeting);
     event.preventDefault();
   }
+    
+  function ClearStorage(){
+    localStorage.clear();
+  }
+
+  btn.addEventListener("click",ClearStorage);
 
 }());
